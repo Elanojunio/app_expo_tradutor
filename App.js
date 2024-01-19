f16cc36026bef1131ab844574379c5f7c305a620
Elano <junioelano@gmail.com>
2024-01-18 23:28:44 -0300

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+export default function app(){
+  return(
+    <View style={Styles.container}>
+      <Text>Tradutor</Text>
     </View>
-  );
+  )
+
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const Styles = StyleSheet.create({
+  container:{
+    flex:1,
+    backgroundColor: "red"
+
+  }
+})
