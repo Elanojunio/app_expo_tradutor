d46ccea8d36f99958b8d38b608fbdc8e06d0c10e
Elano <junioelano@gmail.com>
2024-01-24 23:36:18 -0300

    first commit

--- App.js
+++ App.js
@@ -3,8 +3,17 @@ import { View, Text, StyleSheet } from 'react-native'
 export default function app(){
   return(
     <View style={Styles.container}>
-      <Text>Tradutor</Text>
+
+      <View style={Styles.topbar}> 
+    
     </View>
+    <View>
+      <Text style={Styles.title}>
+        translator
+      </Text>
+    </View>
+    </View>
+    
   )
 
 }
@@ -12,7 +21,24 @@ export default function app(){
 const Styles = StyleSheet.create({
   container:{
     flex:1,
-    backgroundColor: "red"
+    backgroundColor:"gray",
+    margin: "flex",
+    alignItems: 'center',
+    marginBottom:20,
+    height:70,
 
+
+  },
+  topbar: {
+    flexDirection: "row",
+    height: 70,
+    backgroundColor: "#018786"
+  },
+  title: {
+    color: "#c2c2c2S",
+    fontSize: 25,
+    fontWeight: "bold",
+    alignSelf: "center",
+    margin: 30,
   }
 })
